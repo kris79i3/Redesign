90c202dc216d11a9faf4b911efb34369b385e902
hann8085 <hann8085@stud.kea.dk>
2019-12-18 12:49:47 +0100

    menu til desktop virker

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ function clickMenu() {
         document.querySelector("#menuknap").classList = "open";
     }
 }
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        document.querySelector("#menu").classList.remove("hidden");
+
+        document.querySelector("#menu_background").classList.remove("menu_background");
+    } //else {
+    // document.body.style.backgroundColor = "pink";}
+}
+
+var x = window.matchMedia("(min-width: 900px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
